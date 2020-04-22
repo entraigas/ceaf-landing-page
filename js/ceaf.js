fc16725bcb606d1dd5f8651b4fe8429adab0980c
Marcelo Entraigas <entraigas@gmail.com>
2020-04-22 00:16:43 -0300

    mas estilos

--- js/ceaf.js
+++ js/ceaf.js
@@ -4,7 +4,11 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.height = "75px";
+    document.getElementById("header").style.borderWidth = "1px";
+    document.getElementById("header").style.opacity = 0.5;
   } else {
-    document.getElementById("header").style.height = "160px";
+    document.getElementById("header").style.opacity = 1;
+    document.getElementById("header").style.borderWidth = "3px";
+    document.getElementById("header").style.height = "100px";
   }
 }
