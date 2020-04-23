9e86ab0dcd19a4b7f948de9ead106ee6e7dbf5ad
Marcelo Entraigas <entraigas@gmail.com>
2020-04-23 01:22:35 -0300

    el heaader quedo funcional. empece a trabajar en el formulario

--- js/ceaf.js
+++ js/ceaf.js
@@ -3,12 +3,16 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.height = "75px";
+    document.getElementById("header").style.height = "6vmin";
     document.getElementById("header").style.borderWidth = "1px";
-    document.getElementById("header").style.opacity = 0.5;
+    document.getElementById("header").style.opacity = 0.9;
+    document.getElementById("logo").style.maxHeight = "4vmin";
+    document.getElementById("menu").style.paddingTop = "1vmin";
   } else {
-    document.getElementById("header").style.opacity = 1;
+    document.getElementById("header").style.height = "9vmin";
     document.getElementById("header").style.borderWidth = "3px";
-    document.getElementById("header").style.height = "100px";
+    document.getElementById("header").style.opacity = 1;
+    document.getElementById("logo").style.maxHeight = "8vmin";
+    document.getElementById("menu").style.paddingTop = "2vmin";
   }
 }
